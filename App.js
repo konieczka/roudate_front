2008565dee1be6fceb4f48ff79989e69ba019c44
unknown <m.konieczka12@gmail.com>
2020-10-22 23:31:13 +0200

    installed font and added linear gradient to welcome screen

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from "expo-font";
 import WelcomeScreen from "screens/WelcomeScreen";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    Jost: require("./assets/fonts/Jost-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Navigator>
-        <Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
       </Navigator>
     </NavigationContainer>
   );
--- screens/WelcomeScreen/index.js
+++ screens/WelcomeScreen/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 import { View, Text } from "react-native";
 
 const WelcomeScreen = () => (
-  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Text>Welcome Screen</Text>
+  <View
+    style={{
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <LinearGradient
+      colors={["#BD1589", "#2BB6CF"]}
+      style={{
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        height: "100%",
+      }}
+    />
+    <Text style={{ fontSize: 40, color: "white", fontFamily: "Jost" }}>
+      Roudate
+    </Text>
   </View>
 );
 
