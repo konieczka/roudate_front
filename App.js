ec6dfc976fde7f8ff9cd72d5ba655b46f98a426d
Michał Konieczka <m.konieczka12@gmail.com>
2020-11-02 21:06:50 +0100

    Apollo setup

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useFonts } from "expo-font";
 import WelcomeScreen from "screens/WelcomeScreen";
 
 const { Navigator, Screen } = createStackNavigator();
+
+// TODO: add prod/stage url depending on _DEV_ flag value
+const client = new ApolloClient({
+  uri: "localhost:8000/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const [loaded] = useFonts({
@@ -16,14 +23,16 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Navigator>
-        <Screen
-          name="WelcomeScreen"
-          component={WelcomeScreen}
-          options={{ headerShown: false }}
-        />
-      </Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Navigator>
+          <Screen
+            name="WelcomeScreen"
+            component={WelcomeScreen}
+            options={{ headerShown: false }}
+          />
+        </Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
