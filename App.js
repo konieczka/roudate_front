a758b9200f75e2633ae7a36cda6b6163e3a17045
Michał Konieczka <m.konieczka12@gmail.com>
2020-11-06 00:31:47 +0100

    fixed graphql for avd

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, TransitionSpecs } from "@react-navigation/stack";
 import { useFonts } from "expo-font";
 import WelcomeScreen from "screens/WelcomeScreen";
+import RegisterScreen from "screens/RegisterScreen";
 
 const { Navigator, Screen } = createStackNavigator();
 
 // TODO: add prod/stage url depending on _DEV_ flag value
 const client = new ApolloClient({
-  uri: "localhost:8000/graphql",
+  uri: "http://10.0.2.2:8000/graphql",
   cache: new InMemoryCache(),
 });
 
@@ -29,7 +30,16 @@ export default function App() {
           <Screen
             name="WelcomeScreen"
             component={WelcomeScreen}
-            options={{ headerShown: false }}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Screen
+            name="RegisterScreen"
+            component={RegisterScreen}
+            options={{
+              headerShown: false,
+            }}
           />
         </Navigator>
       </NavigationContainer>
