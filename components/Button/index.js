476e4b763b396fb0069d31d8c8ab2228f300afd3
Michał Konieczka <m.konieczka12@gmail.com>
2020-11-18 20:49:00 +0100

    modified Button component to take additional size prop

--- components/Button/index.js
+++ components/Button/index.js
@@ -3,30 +3,68 @@ import PropTypes from "prop-types";
 import { TouchableHighlight, Text } from "react-native";
 import styles from "./styles";
 
-const Button = ({ outlined, label, onPress, buttonColor, labelColor }) => {
+const getButtonSize = (size) => {
+  if (size === "compact") {
+    return { width: 180, height: 30 };
+  } else if (size === "micro") {
+    return { width: 180, height: 30 };
+  }
+
+  return {};
+};
+
+const getLabelSize = (size) => {
+  if (size === "compact") {
+    return { fontSize: 14 };
+  } else if (size === "micro") {
+    return { fontSize: 14 };
+  }
+
+  return {};
+};
+
+const Button = ({
+  outlined,
+  label,
+  onPress,
+  buttonColor,
+  labelColor,
+  size,
+}) => {
+  const buttonSize = getButtonSize(size);
+  const labelSize = getLabelSize(size);
+
+  const styleOutlinedContainer = {
+    ...styles.outline,
+    ...buttonSize,
+    borderColor: buttonColor,
+  };
+
+  const styleFilledContainer = {
+    ...styles.outline,
+    ...buttonSize,
+    borderColor: buttonColor,
+    backgroundColor: buttonColor,
+  };
+
+  const styleOutlinedLabel = {
+    ...styles.labelText,
+    ...labelSize,
+    color: buttonColor,
+  };
+
+  const styleFilledLabel = {
+    ...styles.labelText,
+    ...labelSize,
+    color: labelColor,
+  };
+
   return (
     <TouchableHighlight
-      style={
-        outlined
-          ? styles.outline
-          : {
-              ...styles.outline,
-              borderColor: buttonColor,
-              backgroundColor: buttonColor,
-            }
-      }
+      style={outlined ? styleOutlinedContainer : styleFilledContainer}
       onPress={onPress}
     >
-      <Text
-        style={
-          outlined
-            ? styles.labelText
-            : {
-                ...styles.labelText,
-                color: labelColor,
-              }
-        }
-      >
+      <Text style={outlined ? styleOutlinedLabel : styleFilledLabel}>
         {label}
       </Text>
     </TouchableHighlight>
@@ -39,12 +77,14 @@ Button.propTypes = {
   onPress: PropTypes.func.isRequired,
   buttonColor: PropTypes.string,
   labelColor: PropTypes.string,
+  size: PropTypes.oneOf(["default", "compact", "micro"]),
 };
 
 Button.defaultProps = {
   outlined: false,
   buttonColor: "white",
   labelColor: "#2BB6CF",
+  size: "default",
 };
 
 export default Button;
