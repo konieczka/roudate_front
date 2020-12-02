c51703a0d87f542b961cd1cc9f6cbe51e85dbaf7
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-12-02 17:46:57 +0100

    finalized account verification

---
+++ components/SuccessfullyVerified/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import Button from "components/Button";
+import GradientBackground from "components/GradientBackground";
+import useKeyboard from "hooks/useKeyboard";
+import {
+  ROUDATE_CYAN,
+  ROUDATE_VIOLET,
+  ROUDATE_LIGHT_PINK,
+} from "consts/colors";
+import styles from "./styles";
+
+const SuccessfullyVerified = ({ goToLogin }) => {
+  const isKeyboardVisible = useKeyboard();
+
+  return (
+    <View style={styles.container}>
+      <GradientBackground colors={[ROUDATE_VIOLET, ROUDATE_CYAN]} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={{
+          ...styles.content,
+          paddingBottom: isKeyboardVisible ? 10 : 100,
+        }}
+      >
+        <View
+          style={{
+            ...styles.subsection,
+            display: !isKeyboardVisible ? "flex" : "none",
+          }}
+        >
+          <Text style={styles.header}>
+              Success!
+          </Text>
+          <Text style={styles.subheader}>
+              Your email has been successfully verified. 
+          </Text>
+        </View>
+
+        <Button label="Log in" onPress={goToLogin} />
+        
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
+
+SuccessfullyVerified.propTypes = {
+  goToLogin: PropTypes.func.isRequired,
+};
+
+export default SuccessfullyVerified;
